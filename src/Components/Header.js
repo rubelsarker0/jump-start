bc456409dbd39048decd6ea930e7241afbf8f4bd
Rubel Sarker <74744597+rubelsarker0@users.noreply.github.com>
2024-04-19 11:31:08 +0100

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='font-semibold  shadow-md shadow-slate-300 bg-white sticky top-0 z-50'>
+        <div className='navbar  py-3 container mx-auto'>
+            <div className='text-3xl text-cyan-600 pl-4 font-semibold '>
+                Visualization of Operational Security Data
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
